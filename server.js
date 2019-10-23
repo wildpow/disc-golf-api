f6ed24ff59acb3a251ebfbf25fbedd09bfe1e406
wildpow <element1028@gmail.com>
2019-10-22 18:45:00 -0700

    test

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ var allowedOrigins = ["http://localhost:3000", "http://yourapp.com"];
 //   next();
 // });
 var corsOptions = {
-  origin: "http://localhost:3000/",
+  origin: "http://localhost:3000",
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 app.use(cors(corsOptions));
