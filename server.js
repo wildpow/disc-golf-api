b244a43412a5f9651105b2dcbaf0334e25b2f2cf
wildpow <element1028@gmail.com>
2019-10-22 18:23:43 -0700

    trying to solve cors issue on local host

--- server.js
+++ server.js
@@ -6,7 +6,23 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+var allowedOrigins = ["http://localhost:3000", "http://yourapp.com"];
+app.use(
+  cors({
+    origin: function(origin, callback) {
+      // allow requests with no origin
+      // (like mobile apps or curl requests)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        var msg =
+          "The CORS policy for this site does not " +
+          "allow access from the specified Origin.";
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    }
+  })
+);
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
